0184ac290f09d7fe1bad588e19f635eaa676a18a
Truong Chi Nhan <nhanbin03@gmail.com>
2023-03-30 21:34:39 +0700

    Add State header

---
+++ src/State.h
@@ -0,0 +1,34 @@
+#ifndef STATE_H
+#define STATE_H
+
+#include "StateIdentifiers.h"
+
+#include <memory>
+
+class StateStack;
+
+class State {
+public:
+    typedef std::unique_ptr<State> Ptr;
+    
+    struct Context {
+    };
+
+public:
+    State(StateStack &stack, Context context);
+    virtual ~State();
+
+    virtual bool update(float dt) = 0;
+    virtual void draw() = 0;
+
+protected:
+    void requestStackPush(StateIDs stateID);
+    void requestStackPop();
+    void requestStackClear();
+
+private:
+    StateStack *mStack;
+    Context mContext;
+};
+
+#endif // STATE_H
