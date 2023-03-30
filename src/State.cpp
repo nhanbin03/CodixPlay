d8e55881a499f837f93d4565b644805b5d6a0e4b
Truong Chi Nhan <nhanbin03@gmail.com>
2023-03-30 21:46:04 +0700

    Declare functions for State

--- src/State.cpp
+++ src/State.cpp
@@ -1,4 +1,5 @@
 #include "State.h"
+#include "StateStack.h"
 
 State::State(StateStack &stack, Context context)
 : mStack(&stack)
@@ -6,4 +7,16 @@ State::State(StateStack &stack, Context context)
 }
 
 State::~State() {
+}
+
+void State::requestStackPush(StateIDs stateID) {
+    mStack->pushState(stateID);
+}
+
+void State::requestStackPop() {
+    mStack->popState();
+}
+
+void State::requestStackClear() {
+    mStack->clearStates();
 }
