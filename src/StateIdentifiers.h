b89a2969803568f3137fc18336f76f99b7e44ff4
Truong Chi Nhan <nhanbin03@gmail.com>
2023-03-30 21:33:58 +0700

    Add StateIDs enum

---
+++ src/StateIdentifiers.h
@@ -0,0 +1,16 @@
+#ifndef STATEIDENTIFIERS_H
+#define STATEIDENTIFIERS_H
+
+enum class StateIDs {
+    None,
+    Home,
+    StaticArray,
+    DynamicArray,
+    SinglyLinkedList,
+    DoublyLinkedList,
+    CircularLinkedList,
+    Stack,
+    Queue
+};
+
+#endif // STATEIDENTIFIERS_H
