491842e2fdb7d6f4948f9aa769a93bd6f70fc5d9
Truong Chi Nhan <nhanbin03@gmail.com>
2023-04-02 14:24:37 +0700

    Update setter and getter for GUI

---
+++ src/GUIComponent.cpp
@@ -0,0 +1,17 @@
+#include "GUIComponent.h"
+
+Vector2 GUIComponent::getPosition() const {
+    return mPosition;
+}
+
+void GUIComponent::setPosition(Vector2 position) {
+    mPosition = position;
+}
+
+Vector2 GUIComponent::getSize() const {
+    return mSize;
+}
+
+void GUIComponent::setSize(Vector2 size) {
+    mSize = size;
+}
--- src/GUIComponent.h
+++ src/GUIComponent.h
@@ -1,10 +1,22 @@
 #ifndef GUICOMPONENT_H
 #define GUICOMPONENT_H
 
+#include "raylib.h"
+
 class GUIComponent {
 public:
     virtual void update(float dt) = 0;
     virtual void draw() = 0;
+
+    Vector2 getPosition() const;
+    void setPosition(Vector2 position);
+
+    Vector2 getSize() const;
+    void setSize(Vector2 size);
+
+protected:
+    Vector2 mPosition;
+    Vector2 mSize;
 };
 
 #endif // GUICOMPONENT_H
