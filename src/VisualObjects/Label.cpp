6c7f7b78c73cc926b733f0aeb3fded1c354f8886
Truong Chi Nhan <nhanbin03@gmail.com>
2023-04-13 21:23:15 +0700

    Add scaling to Label

--- src/VisualObjects/Label.cpp
+++ src/VisualObjects/Label.cpp
@@ -8,9 +8,10 @@ Label::~Label() {
 }
 
 void Label::draw() {
+    int fontSize = mSize * getScale();
     DrawText(mText.c_str(),
-             getPosition().x - MeasureText(mText.c_str(), mSize) / 2,
-             getPosition().y - mSize / 2, mSize, mColor);
+             getPosition().x - MeasureText(mText.c_str(), fontSize) / 2,
+             getPosition().y - fontSize / 2, fontSize, mColor);
 }
 
 void Label::setText(std::string text) {
