6a7072f65716fadbd347daefaf0274f3f927ecc5
Truong Chi Nhan <nhanbin03@gmail.com>
2023-04-15 17:08:17 +0700

    Add app color palette

---
+++ src/ColorPalettes/AppColorPalette.h
@@ -0,0 +1,18 @@
+#ifndef COLORPALETTES_APPCOLORPALETTE_H
+#define COLORPALETTES_APPCOLORPALETTE_H
+
+#include "raylib.h"
+
+namespace AppColor {
+    const Color BACKGROUND_1 = GetColor(0x1E1E1E);
+    const Color BACKGROUND_2 = GetColor(0x252525);
+    const Color BACKGROUND_3 = GetColor(0x2D2D2D);
+    const Color BACKGROUND_4 = GetColor(0x3C3C3C);
+
+    const Color TEXT = GetColor(0xC4C4C4);
+    const Color TEXT_ACCENT = GetColor(0xFFFFFF);
+
+    const Color PRIMARY = GetColor(0x0C8CE9);
+} // namespace AppColor
+
+#endif // COLORPALETTES_APPCOLORPALETTE_H
