275ca56232e8fb993322ae0c9794a7d0320a8da9
Truong Chi Nhan <nhanbin03@gmail.com>
2023-04-15 17:16:04 +0700

    Correct app color

--- src/ColorPalettes/AppColorPalette.h
+++ src/ColorPalettes/AppColorPalette.h
@@ -4,15 +4,15 @@
 #include "raylib.h"
 
 namespace AppColor {
-    const Color BACKGROUND_1 = GetColor(0x1E1E1E);
-    const Color BACKGROUND_2 = GetColor(0x252525);
-    const Color BACKGROUND_3 = GetColor(0x2D2D2D);
-    const Color BACKGROUND_4 = GetColor(0x3C3C3C);
+    const Color BACKGROUND_1 = GetColor(0x1E1E1EFF);
+    const Color BACKGROUND_2 = GetColor(0x252525FF);
+    const Color BACKGROUND_3 = GetColor(0x2D2D2DFF);
+    const Color BACKGROUND_4 = GetColor(0x3C3C3CFF);
 
-    const Color TEXT = GetColor(0xC4C4C4);
-    const Color TEXT_ACCENT = GetColor(0xFFFFFF);
+    const Color TEXT = GetColor(0xC4C4C4FF);
+    const Color TEXT_ACCENT = GetColor(0xFFFFFFFF);
 
-    const Color PRIMARY = GetColor(0x0C8CE9);
+    const Color PRIMARY = GetColor(0x0C8CE9FF);
 } // namespace AppColor
 
 #endif // COLORPALETTES_APPCOLORPALETTE_H
