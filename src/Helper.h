a38557c3d27a24c67f13036d138990d12c31a9b8
Truong Chi Nhan <nhanbin03@gmail.com>
2023-04-26 09:43:33 +0700

    Implement file reading function

--- src/Helper.h
+++ src/Helper.h
@@ -3,6 +3,9 @@
 
 #include "raylib.h"
 
+#include <fstream>
+#include <string>
+
 Vector2 operator+ (const Vector2 &a, const Vector2 &b);
 
 Vector2 operator- (const Vector2 &a, const Vector2 &b);
@@ -13,4 +16,20 @@ Vector2 operator/ (const Vector2 &a, float x);
 
 void scaleTexture(Texture2D &source, Vector2 newSize);
 
+template <typename T>
+std::vector<T> readListFromFile(std::string filePath);
+
 #endif // HELPER_H
+
+template <typename T>
+std::vector<T> readListFromFile(std::string filePath) {
+    std::ifstream fin;
+    fin.open(filePath);
+    std::vector<T> ret;
+    T input;
+    while (fin >> input) {
+        ret.push_back(input);
+    }
+    fin.close();
+    return ret;
+}
